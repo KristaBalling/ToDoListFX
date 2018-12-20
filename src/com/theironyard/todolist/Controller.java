017eaff934733cdc1a6b70d65fc9a0a2f5e8db1a
Krista Blachian <krista.blachian@scifusion360.com>
2018-12-20 16:46:34 -0500

    formatted due date

--- src/com/theironyard/todolist/Controller.java
+++ src/com/theironyard/todolist/Controller.java
@@ -10,6 +10,7 @@ import javafx.scene.control.TextArea;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -49,6 +50,9 @@ public class Controller {
                 if(newValue != null) {
                     TodoItem item = todoListView.getSelectionModel().getSelectedItem();
                     itemDetailsTextArea.setText(item.getDetails());
+                    deadlineLabel.setText(item.getDeadline().toString());
+                    DateTimeFormatter df = DateTimeFormatter.ofPattern("MMMM d, yyyy");
+                    deadlineLabel.setText(df.format(item.getDeadline()));
                 }
             }
         });
