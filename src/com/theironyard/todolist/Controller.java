412a46201bae777b9e2271f72b3dd844c6132a95
Krista Blachian <krista.blachian@scifusion360.com>
2019-01-03 15:12:51 -0500

    added highlighting feature to toggle button, added exit button functionality

--- src/com/theironyard/todolist/Controller.java
+++ src/com/theironyard/todolist/Controller.java
@@ -2,6 +2,7 @@ package com.theironyard.todolist;
 
 import com.theironyard.todolist.datamodel.TodoData;
 import com.theironyard.todolist.datamodel.TodoItem;
+import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.transformation.FilteredList;
@@ -51,6 +52,11 @@ public class Controller {
     @FXML
     private FilteredList<TodoItem> filteredList;
 
+    private Predicate<TodoItem> wantAllItems;
+    private Predicate<TodoItem> wantTodaysItems;
+
+
+
     public void initialize() {
 
         listContextMenu = new ContextMenu();
@@ -76,13 +82,21 @@ public class Controller {
             }
         });
 
-        filteredList = new FilteredList<TodoItem>(TodoData.getInstance().getTodoItems(),
-                new Predicate<TodoItem>() {
-                    @Override
-                    public boolean test(TodoItem todoItem) {
-                        return true;
-                    }
-                });
+        wantAllItems = new Predicate<TodoItem>() {
+            @Override
+            public boolean test(TodoItem todoItem) {
+                return true;
+            }
+        };
+
+        wantTodaysItems = new Predicate<TodoItem>() {
+            @Override
+            public boolean test(TodoItem todoItem) {
+                return (todoItem.getDeadline().equals(LocalDate.now()));
+            }
+        };
+
+        filteredList = new FilteredList<TodoItem>(TodoData.getInstance().getTodoItems(), wantAllItems);
 
         SortedList<TodoItem> sortedList = new SortedList<TodoItem>(filteredList,
                 new Comparator<TodoItem>() {
@@ -197,21 +211,27 @@ public class Controller {
     }
 
     public void handleFilterButton() {
+        TodoItem selectedItem = todoListView.getSelectionModel().getSelectedItem();
         if (filterToggleButton.isSelected()) {
-            filteredList.setPredicate(new Predicate<TodoItem>() {
-                @Override
-                public boolean test(TodoItem todoItem) {
-                    return (todoItem.getDeadline().equals(LocalDate.now()));
-                }
-            });
+            filteredList.setPredicate(wantTodaysItems);
+            if(filteredList.isEmpty()) {
+              itemDetailsTextArea.clear();
+              deadlineLabel.setText("");
+            } else if (filteredList.contains(selectedItem)) {
+                todoListView.getSelectionModel().select(selectedItem);
+            } else {
+                todoListView.getSelectionModel().selectFirst();
+            }
 
         } else {
-            filteredList.setPredicate(new Predicate<TodoItem>() {
-                @Override
-                public boolean test(TodoItem todoItem) {
-                    return true;
-                }
-            });
+            filteredList.setPredicate(wantAllItems);
+            todoListView.getSelectionModel().select(selectedItem);
         }
+    }
+
+    @FXML
+    public void handleExit() {
+        Platform.exit();
+
     }
 }
